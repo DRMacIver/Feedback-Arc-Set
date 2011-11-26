2db6807198d65099457feb5655567b64a471e9de
David R. MacIver <david@drmaciver.com>
2011-11-26 09:02:00 +0000

    change output format and randomzation of fas

--- fas.c
+++ fas.c
@@ -1,16 +1,21 @@
+#include <time.h>
+#include <unistd.h>
+
 #include "tournament.h"
 #include "fas_tournament.h"
 
 int main(){
+  srand(time(NULL) ^ getpid());
 	tournament *t = read_tournament(stdin);
 
 	fas_tournament *ft = run_fas_tournament(t);
 
 	printf("Score: %f\n", ft->score);
-	printf("Optimal ordering:\n");
+	printf("Optimal ordering:");
 	for(size_t i = 0; i < ft->results; i++){
-		printf("  %lu\n", ft->optimal_ordering[i]);
+		printf(" %lu", ft->optimal_ordering[i]);
 	}
+  printf("\n");
 
 	del_fas_tournament(ft);
 	del_tournament(t);
