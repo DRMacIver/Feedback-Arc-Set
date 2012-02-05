abece4543ec0e9ab46d932bd4e0f2cb5e874df2b
David R. MacIver <david@drmaciver.com>
2012-02-05 13:41:52 +0000

    use ranked pairs. It appears to produce slightly better results for some test cases

--- fas.py
+++ fas.py
@@ -1,7 +1,10 @@
 import random
 import math
+from functools import total_ordering
 
-def score_fas_tournament(weights, elems):
+# Public API
+
+def score(weights, elems):
   score = 0.0
 
   for i in xrange(len(elems)):
@@ -14,39 +17,81 @@ def optimise(weights, elems = None):
   if not elems:
     elems = xrange(weights.size)
 
-  elems = list(elems)
-  best_so_far = list(elems)
-  best_score = score_fas_tournament(weights, elems)
 
-  fail_count = 0
-
-  for i in xrange(10000):
-    random.shuffle(elems)
-    new_score = score_fas_tournament(weights, elems)
-    if new_score > best_score:
-      fail_count = 0
-      best_so_far = list(elems)
-      best_score = new_score
-    else:
-      fail_count += 1
-      if fail_count > 100: break
-
-  return best_so_far, best_score
+  @total_ordering
+  class WeightKey(object):
+    def __init__(self, value):
+      self.value = value
+    def __gt__(self, other):
+      return weights.get(self.value, other.value) < weights.get(other.value, self.value)
+    def __eq__(self, other):
+      return self.value == other.value
+  
+  return sorted(elems, key = WeightKey)
     
-def lower_bound(weights):
+def lower_bound(weights, elems = None):
+  if not elems: elems = range(weights.size)
+
   tot = 0.0;
   vtot = 0.0;
 
-  for i in xrange(weights.size):
-    for j in xrange(i + 1, weights.size):
-      aij = weights.get(i, j)
-      aji = weights.get(j, i)
+  for i, j in pairs(elems):
+    aij = weights.get(i, j)
+    aji = weights.get(j, i)
 
-      tot += aij
-      tot += aji
+    tot += aij
+    tot += aji
 
-      vtot += (aij - aji) * (aij - aji);
+    vtot += (aij - aji) ** 2
 
   return 0.5 * tot + 0.5 * math.sqrt(vtot);
 
+# Module private stuff
 
+def ranked_pairs(weights, elems):
+  edges = []
+
+  for x, y in pairs(elems):
+    wxy = weights.get(x, y)
+    wyx = weights.get(y, x)
+
+    if wxy > wyx: edges.append((x, y, wxy - wyx))
+    if wxy < wyx: edges.append((y, x, wyx - wxy))
+
+  edges.sort(key = lambda p: -p[2])
+
+  lesser = {}
+  greater = {}
+
+  for x in elems: 
+    lesser[x] = set()
+    greater[x] = set()
+
+  def add_to_transitive_graph(x, y):
+    if x == y: return
+    if y in lesser[x] or x in lesser[y]: return
+
+    lesser[y].add(x)
+    greater[x].add(y)
+
+    for z in greater[y]: add_to_transitive_graph(x, z)
+    for z in lesser[x]: add_to_transitive_graph(z, y)
+
+  for x, y, _ in edges:
+    add_to_transitive_graph(x, y)
+
+  @total_ordering
+  class GraphKey(object):
+    def __init__(self, value):
+      self.value = value
+    def __gt__(self, other):
+      return other.value in lesser[self.value]
+    def __eq__(self, other):
+      return self.value == other.value
+  
+  return sorted(elems, key = GraphKey)
+
+def pairs(elems):
+  for i in xrange(len(elems)):
+    for j in xrange(i + 1, len(elems)):
+      yield elems[i], elems[j]
